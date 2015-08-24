ba2f0e8207a8e55684eda08dc417561580f95095
Lucas Marcal <lucasmarcaldossantos@gmail.com>
2015-08-23 23:20:46 -0300

    a more usefull version using streams so you can '.pipe' it. Dependecies changed.

--- index.js
+++ index.js
@@ -1,51 +1,24 @@
-var fs = require('fs'),
-	path = require('path'),
-	crypt = require('crypto');
+var path = require('path'),
+    crypt = require('crypto'),
+    gutil = require('gulp-util'),
+    through = require('through2'),
+    PluginError = gutil.PluginError;
 
+const PLUGIN_NAME = 'gulp-bustcache';
 
+function gulpBustCache() {
+	return through.obj(function(file, encode, callback) {
+		var ext = path.extname(file.path),
+		    name = path.basename(file.path, ext),
+		    dir = path.dirname(file.path),
+		    rand = Math.round(Math.random() * +new Date()).toString(),
+		    md5sum = crypt.createHash('md5'),
+		    hash = md5sum.update(rand).digest('hex');
 
-module.exports = function(arg, opts) {
+		file.path = dir + "/" + name + "." + hash + ext;
 
-	var md5Sum = null,
-		dateStr = (+new Date()).toString(),
-		arr = (typeof arg === "string") ? [arg] : (typeof arg === "object") ? arg : undefined,
-		hash = '', ext = '', replaceArray = [];
-	
-	if (!arr)
-		throw new Error("Invalid parameters. String or array expected, " + typeof arg + " given.");
-
-	// fs.createReadStream(opts.src + opts.replace).pipe(fs.createWriteStream(opts.dist + opts.replace));
-
-	for (var file in arr) {
-		md5Sum = crypt.createHash('md5');
-		hash = md5Sum.update(dateStr + arr[file]).digest('hex');
-		
-		srcFile = opts.src + arr[file];
-		ext = path.extname(srcFile);
-		distFile = path.basename(srcFile, ext) + '.' + hash + ext
-		
-		fs.createReadStream(srcFile).pipe(fs.createWriteStream(opts.dist + distFile));
-
-		replaceArray.push({
-			regex: new RegExp("\\{" + path.basename(srcFile) + "\\}", "g"),
-			replace: distFile
-		});
-	}
-
-	fs.readFile(opts.src + opts.replace, 'utf8', function (err,data) {
-		var result = data,
-		    regex = undefined;
-		if (err) {
-			return console.log(err);
-		}
-		
-
-		replaceArray.forEach(function(v, k) {
-			result = result.replace(v.regex, v.replace);
-		});
-
-		fs.writeFile(opts.dist + opts.replace, result, 'utf8', function (err) {
-			if (err) return console.log(err);
-		});
+		callback(null, file);
 	});
 }
+
+module.exports = gulpBustCache;
